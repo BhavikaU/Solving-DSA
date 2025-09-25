843be7e4a29f22f66ffe6537103951f6abd13feb
BhavikaU <uppalabhavika04@gmail.com>
2025-09-25 17:03:34 +0530

    Updated with Optimal Approch

--- Arrays/Largest_Element.java
+++ Arrays/Largest_Element.java
@@ -1,13 +1,32 @@
+// package Arrays;
 import java.util.*;
-
+//with sorting
 public class Largest_Element {
-    static int sort(int arr[]) {  // ✅ Change return type to int
+    static int largestelement1(int arr[]) {  // ✅ Change return type to int
         Arrays.sort(arr);
         return arr[arr.length - 1];  // ✅ Returns largest element
+
     }
+    //With out sorting
+    static int largestelement2(int arr[])
+    {
+        int max = arr[0];
+        for(int i=1; i < arr.length; i++)
+        {
+            if(arr[i] > max)
+            {
+                max = arr[i];
+
+            }
+        }
+        return max;
+    }
+    
+    
 
     public static void main(String[] args) {
         int[] arr1 = {5, 85, 96, 102};
-        System.out.println("Largest element is: " + sort(arr1));
+        System.out.println("Largest element is: " + largestelement1(arr1));
+        System.out.println("Largest element is:(without sorting) " + largestelement2(arr1));
     }
 }
