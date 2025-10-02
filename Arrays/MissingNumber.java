7ecac9767b9f3d0c095cfdfd2543bfbf880c6965
BhavikaU <uppalabhavika04@gmail.com>
2025-10-02 17:31:00 +0530

    Optimal Approch

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -7,8 +7,10 @@ public static void main(String[] args)
    int arr[] = {1,2,4,5};
 
    int ans = missingnumber1(arr,N);
+    int ans1 = missingnumber2(arr,N);
 
    System.out.println("This is the Missing Number " + ans );
+   System.out.println("This is the Missing Number " + ans1 );
 }
 
 // Brute Approch -> Linear Search
@@ -32,4 +34,20 @@ public static int missingnumber1(int arr[] , int N)
     }
     return -1; // return -1 if no number is missing (edge case)
 }
+
+//Optimal Approch
+
+public static int missingnumber2(int arr[], int N)
+{
+    int sum1 = (N * (N + 1)) / 2;
+    int sum2 = 0;
+    for(int i=0;i<N-1; i++)
+    {
+        sum2 = sum2 + arr[i];
+    }
+
+    int missing = sum1 - sum2;
+    return missing;
+}
+
 }
